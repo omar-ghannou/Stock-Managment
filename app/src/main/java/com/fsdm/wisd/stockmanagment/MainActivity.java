0aae4dea95afa6fcc9a8bf11ac83b53ec7a2c4cf
Hassan <hassanelkharroubi1997@gmail.com>
2021-01-30 15:29:40 +0100

    add listener for a selected itenm in spinner

--- app/src/main/java/com/fsdm/wisd/stockmanagment/MainActivity.java
+++ app/src/main/java/com/fsdm/wisd/stockmanagment/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner mSpinner;
     DatabaseHelper mydb;
@@ -22,9 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
         mydb = new DatabaseHelper(getBaseContext());
         String []category={"Choose category","Computer","Clavier","Telephone"};
-        mArrayAdapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,category);
+        mArrayAdapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,category);
         mSpinner=findViewById(R.id.spinner);
         mSpinner.setAdapter(mArrayAdapter);
+        mSpinner.setOnItemSelectedListener(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        //lsit all products that belong to this category
+        //adapterView.getItemAtPosition(i)
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
